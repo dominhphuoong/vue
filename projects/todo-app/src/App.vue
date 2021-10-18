36db2d086f0b7d225d1a6143337614577671b340
phuongdm <phuongdm@Phuongdm-Latitude-E7480>
2021-10-18 16:30:18 +0700

    complete todo-app project

--- projects/todo-app/src/App.vue
+++ projects/todo-app/src/App.vue
@@ -1,17 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <create-todo v-on:add-todo="addTodo" />
+    <todo-list v-bind:todos="todos"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoList from './components/TodoList.vue'
+import CreateTodo from './components/CreateTodo.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoList,
+    CreateTodo
+  },
+  methods: {
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  data () {
+    return {
+      todos: [
+        {
+          title: 'To do A',
+          project: 'Project A',
+          done: true
+        },
+        {
+          title: 'To do B',
+          project: 'Project B',
+          done: false
+        },
+        {
+          title: 'To do C',
+          project: 'Project C',
+          done: false
+        },
+        {
+          title: 'To do D',
+          project: 'Project D',
+          done: false
+        }
+      ]
+    }
   }
 }
 </script>
@@ -21,7 +53,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
 }
